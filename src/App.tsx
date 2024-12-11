51d3c5badf8634aa8aac002f22590e8ab276e857
odinokun <odinokun@gmail.com>
2024-12-10 21:57:32 -0500

    refactor: refactor App component to demonstrate object and array copying techniques

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,177 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  // 1. Simple object
+  const man = {
+    name: 'John',
+    age: 28,
+  };
+  console.log('man', man.age);
+  const manFullCopy = { ...man }; // your code
+  console.log('manFullCopy', manFullCopy.age);
+
+  // 2. Array of primitives
+  let numbers = [1, 2, 3];
+
+  let numbersFullCopy; //  your code
+
+  // 3. Object inside an object
+  let man1 = {
+    name: 'John',
+    age: 28,
+    mother: {
+      name: 'Kate',
+      age: 50,
+    },
+  };
+
+  let man1FullCopy; // your code
+
+  // 4. Array of primitives inside an object
+  let man2 = {
+    name: 'John',
+    age: 28,
+    friends: ['Peter', 'Steven', 'William'],
+  };
+
+  let man2FullCopy; // your code
+
+  // 5 Array of objects
+  let people = [
+    { name: 'Peter', age: 30 },
+    { name: 'Steven', age: 32 },
+    { name: 'William', age: 28 },
+  ];
+
+  let peopleFullCopy; // your code
+
+  // 6 Array of objects inside object
+  let man3 = {
+    name: 'John',
+    age: 28,
+    friends: [
+      { name: 'Peter', age: 30 },
+      { name: 'Steven', age: 32 },
+      { name: 'William', age: 28 },
+    ],
+  };
+
+  let man3FullCopy; //  your code
+
+  // 7 Object inside an object, inside an object
+  let man4 = {
+    name: 'John',
+    age: 28,
+    mother: {
+      name: 'Kate',
+      age: 50,
+      work: {
+        position: 'doctor',
+        experience: 15,
+      },
+    },
+  };
+
+  let man4FullCopy; //  your code
+
+  // 8 Array of objects inside object -> object
+  let man5 = {
+    name: 'John',
+    age: 28,
+    mother: {
+      name: 'Kate',
+      age: 50,
+      work: {
+        position: 'doctor',
+        experience: 15,
+      },
+      parents: [
+        { name: 'Kevin', age: 80 },
+        { name: 'Jennifer', age: 78 },
+      ],
+    },
+  };
+
+  let man5FullCopy; //  your code
+
+  // 9 Object inside an object -> array -> object ->  object
+  let man6 = {
+    name: 'John',
+    age: 28,
+    mother: {
+      name: 'Kate',
+      age: 50,
+      work: {
+        position: 'doctor',
+        experience: 15,
+      },
+      parents: [
+        {
+          name: 'Kevin',
+          age: 80,
+          favoriteDish: {
+            title: 'borscht',
+          },
+        },
+        {
+          name: 'Jennifer',
+          age: 78,
+          favoriteDish: {
+            title: 'sushi',
+          },
+        },
+      ],
+    },
+  };
+
+  let man6FullCopy; //  your code
+
+  //10 Array of objects inside an object -> object -> array -> object ->  object
+  let man7 = {
+    name: 'John',
+    age: 28,
+    mother: {
+      name: 'Kate',
+      age: 50,
+      work: {
+        position: 'doctor',
+        experience: 15,
+      },
+      parents: [
+        {
+          name: 'Kevin',
+          age: 80,
+          favoriteDish: {
+            title: 'borscht',
+            ingredients: [
+              { title: 'beet', amount: 3 },
+              { title: 'potatoes', amount: 5 },
+              { title: 'carrot', amount: 1 },
+            ],
+          },
+        },
+        {
+          name: 'Jennifer',
+          age: 78,
+          favoriteDish: {
+            title: 'sushi',
+            ingredients: [
+              { title: 'fish', amount: 1 },
+              { title: 'rise', amount: 0.5 },
+            ],
+          },
+        },
+      ],
+    },
+  };
+
+  let man7FullCopy; //  your code
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Vite</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
